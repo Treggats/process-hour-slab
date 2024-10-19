48d6c08965a627e8791161b399de531685b8646d
Tonko Mulder <tonko@tonkomulder.nl>
2024-10-19 22:59:01 +0200

    basic views / layout / controller
    Signed-off-by: Tonko Mulder <tonko@tonkomulder.nl>

---
+++ app/Views/components/x-navigation.view.php
@@ -0,0 +1,10 @@
+<x-component name="navigation">
+    <nav class="flex flex-col items-center max-w-lg py-2 mx-auto">
+        <ul class="flex gap-6">
+            <li>item 1</li>
+            <li>item 2</li>
+            <li>item 3</li>
+            <li>item 4</li>
+        </ul>
+    </nav>
+</x-component>
